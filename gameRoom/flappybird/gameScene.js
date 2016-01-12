5674537acb8ceb1015989b9ce57131ec0aec767b
zhong0902 <785861210@qq.com>
2016-01-12 16:09:27 +0800

    dev

--- gameRoom/flappybird/gameScene.js
+++ gameRoom/flappybird/gameScene.js
@@ -2,10 +2,22 @@ var size = {
     "width" : 640,
     "height" : 1280
 }
+var k_Acceleration = 2;
+var globalSpr = null;
 var gameScene = cc.Scene.extend({
     PipeSpriteList:[],
+    m_DeltaY:0,
+    m_startPosition:null,
+    m_targetPosition:null,
+
+    tick:0,
+
+    passTime:0,
     ctor:function(){
         this._super();
+
+        this.m_startPosition = cc.p(0, 0);
+        this.m_targetPosition = cc.p(0, 0);
 
         this.init();
 
@@ -15,11 +27,7 @@ var gameScene = cc.Scene.extend({
 
         this.PipeSpriteList = [];
 
-        this.addPipe();
-
         this.initBird();
-
-        // this.runBirdAction();
 
         this.loadListener();
     },
@@ -56,6 +64,22 @@ var gameScene = cc.Scene.extend({
     update:function(){
         // this.bg.update(0,-840,0);
         // this.bg1.update(0,0,840);
+
+        for(var i = 0; i < this.PipeSpriteList.length; ++ i) {
+            var pipe = this.PipeSpriteList[i];
+            pipe.setPositionX(pipe.getPositionX() - 3);
+            if (pipe.getPositionX() < -pipe.getContentSize().width / 2) {
+                this.PipeSpriteList.splice(i, 1);
+            }
+        }
+
+        this.passTime += 1;
+        if(this.passTime >= size.width/4){
+            this.addPipe();
+            this.passTime = 0;
+        }
+
+        this.checkCollision();
     },
     initReady:function(){
         this.bird = new cc.Sprite(res.bird);
@@ -64,31 +88,24 @@ var gameScene = cc.Scene.extend({
         this.addChild(this.bird);
     },
     initBird:function(){
-        var animation = cc.animationCache.getAnimation("FlyBirdAnimation");
+        var texture = cc.textureCache.addImage(res.ani);//添加纹理到缓存
+        
+        var sprFrames = [];
+        sprFrames[0] = new cc.SpriteFrame(texture,cc.rect(100 * 0,0,100,148));
+        sprFrames[1] = new cc.SpriteFrame(texture,cc.rect(100 * 1,0,100,148));
+        var animation = new cc.Animation(sprFrames,0.3);
+        var animate = new cc.animate(animation);
+        // var repeat = animate.repeatForever();
 
-        if(!animation){
-            var animFrames = [];
-            var str = "";
-            var birdFrameCount = 4;
-            for(var i=0; i<birdFrameCount; i++){
-                str = "bird" +i+ ".png";
-                var frame = cc.spriteFrameCache.getSpriteFrame(str);
-                animFrames.push(frame);
-            }
-        }
-
-        console.log(cc.spriteFrameCache.getSpriteFrame("bird1.png"))
-
-        animation = new cc.Animation(animFrames, 0.05, true);
         cc.animationCache.addAnimation(animation, "FlyBirdAnimation");
 
-        this.flyBird = new cc.Sprite(res.bird);
+        globalSpr = this.flyBird = new cc.Sprite(res.bird);
         this.flyBird.x = size.width/2;
         this.flyBird.y = size.height/2;
         this.addChild(this.flyBird);
 
         // var actionFrame = cc.Animate(animation); 
-        // var flyAction = new cc.RepeatForever(actionFrame);
+        var flyAction = new cc.repeatForever(animate);
 
         // this.flyBird.runAction(flyAction);
     },
@@ -97,46 +114,99 @@ var gameScene = cc.Scene.extend({
         var birdX = this.flyBird.getPositionX(); 
         var birdY = this.flyBird.getPositionY(); 
         // var bottomY = this.groundSprite.getContentSize().height - this.flyBird.getContentSize().height / 2; 
-        var bottomY = 700;
+        var bottomY = 250;
       
         // var actionFrame = cc.Animate.create(cc.AnimationCache.getInstance().getAnimation("FlyBirdAnimation")); 
         // var flyAction = cc.RepeatForever.create(actionFrame); 
-    //上升动画 
+        //上升动画 
         var riseMoveAction = cc.MoveTo.create(0.2, cc.p(birdX, birdY + riseHeight)); 
         var riseRotateAction = cc.RotateTo.create(0, -30); 
         var riseAction = cc.Spawn.create(riseMoveAction, riseRotateAction); 
-    //下落动画 
-    //模拟自由落体运动 
+        //下落动画 
+        //模拟自由落体运动 
         var fallMoveAction = FreeFall.create(birdY - bottomY); 
+        // var bb = birdY - bottomY;
+        // var fallMoveAction = new cc.callFunc(this.test(bb),this);
         var fallRotateAction =cc.RotateTo.create(0, 30); 
-        var fallAction = cc.Spawn.create(fallMoveAction, fallRotateAction); 
-        // this.flyBird.stopAllActions(); 
+        var fallAction = new cc.Spawn(fallMoveAction, fallRotateAction); 
+        this.flyBird.stopAllActions(); 
         // this.flyBird.runAction(flyAction); 
 
-        var seq = new cc.sequence(riseAction, fallAction)
+        var spaseq = new cc.sequence(riseAction, fallAction)
         // this.flyBird.runAction(cc.Spawn.create( 
         //     cc.Sequence.create(riseAction, fallAction) ) 
         // );
 
-        this.flyBird.runAction(seq)
+        this.flyBird.runAction(new cc.Spawn(new cc.sequence(riseAction, fallAction)));
     },
     addPipe:function(){
-        var upPipe = new cc.Sprite(res.up);
-        upPipe.anchorY = 1;
-        upPipe.scaleY = 0.5;
-        upPipe.x = size.width/2 + upPipe.getContentSize().width/2;
-        upPipe.y = size.height + 100;
-
-        this.addChild(upPipe);
-
-
         var downPipe = new cc.Sprite(res.down);
-        downPipe.anchorY = 0;
-        downPipe.scaleY = 0.5;
-        downPipe.x = upPipe.x;
-        downPipe.y = -60;
+        var pipeHeight = downPipe.getContentSize().height;
+        var pipeWidth = downPipe.getContentSize().width;
+        var groundHeight = 250;//滚动背景的高度
 
-        this.addChild(downPipe);
+        var acrossHeight = 300;//小鸟飞行区间高度
+        var downPipeHeight = 100 + getRandom(400);
+
+        var upPipeHeight = size.height - downPipeHeight - acrossHeight - groundHeight;
+        var pipeX = size.width + pipeWidth/2;
+        downPipe.x = pipeX + pipeWidth/2;
+        downPipe.y = groundHeight + pipeHeight/2 - (pipeHeight - downPipeHeight);
+
+
+        var upPipe = new cc.Sprite(res.up);
+        upPipe.x = pipeX + pipeWidth/2;
+        upPipe.y = size.height + (pipeHeight - upPipeHeight) - pipeHeight/2;
+
+        this.addChild(downPipe,98);
+        this.addChild(upPipe,98);
+
+        this.PipeSpriteList.push(downPipe);
+        this.PipeSpriteList.push(upPipe);
+    },
+    getRect:function(spr){
+        var pos = spr.getPosition();
+        var content = spr.getContentSize();
+
+        return cc.rect(pos.x - content.width/2,pos.y - content.height/2,content.width,content.height);
+    },
+    collide:function(a, b){
+        var aRect = this.getRect(a);
+        var bRect = this.getRect(b);
+        return cc.rectIntersectsRect(aRect, bRect);
+    },
+    checkCollision:function(){
+        // if(this.collide(this.flyBird, this.groundSprite)){
+        //     //console.log("hit floor");
+        //     this.birdFallAction();
+        //     return;
+        // }
+        for(var i=0;i<this.PipeSpriteList.length;i++){
+            var pipe = this.PipeSpriteList[i];
+            if(this.collide(this.flyBird, pipe)){
+                // console.log("hit pipe i="+i);
+                this.birdFallAction();
+                break;
+            }
+        }
+    },
+    birdFallAction:function(){
+        this.flyBird.stopAllActions();
+        var birdX = this.flyBird.getPositionX();
+        var birdY = this.flyBird.getPositionY();
+
+        // var bottomY = this.groundSprite.getContentSize().height + this.flyBird.getContentSize().width/2;
+        var bottomY = 250;
+        var fallMoveAction = FreeFall.create(birdY - bottomY);
+        var fallRotateAction = new cc.RotateTo(0,90);
+        var fallAction = new cc.Spawn(fallMoveAction, fallRotateAction);
+        this.flyBird.runAction(new cc.sequence(new cc.delayTime(0.1),fallAction));
+
+        this.runAction(new cc.sequence(new cc.delayTime(1), new cc.callFunc(this.gameOver, this)));
+    },
+    gameOver:function(){
+        console.log('gameOver');
+        this.unscheduleUpdate();
     },
     loadListener : function(){
         var listener = cc.EventListener.create({
@@ -168,19 +238,16 @@ var gameScene = cc.Scene.extend({
         // var tag = target.tag;
         // var tag = self.tag;
 
-        console.log('ok')
-
         self.runBirdAction();
     }
 });
 
-k_Acceleration = 2;
-var FreeFall = cc.ActionInterval.extend( {
-     timeElasped:0,
-     m_positionDeltaY:null,
-     m_startPosition:null,
-     m_targetPosition:null,
 
+var FreeFall = cc.ActionInterval.extend( {
+    timeElasped:0,
+    m_positionDeltaY:null,
+    m_startPosition:null,
+    m_targetPosition:null,
     ctor:function() {
         cc.ActionInterval.prototype.ctor.call(this);
         this.yOffsetElasped = 0;
@@ -188,72 +255,67 @@ var FreeFall = cc.ActionInterval.extend( {
         this.m_positionDeltaY = 0;
         this.m_startPosition = cc.p(0, 0);
         this.m_targetPosition = cc.p(0, 0);
-     },
-
-    initWithDuration:function (duration) {
+    },
+    initWithDuration:function (duration) {//是设置该action运行的时间，时间的长短决定下降的速度。
         if (cc.ActionInterval.prototype.initWithDuration.call(this, duration)) {
             return true;
         }
         return false;
     },
-
     initWithOffset:function(deltaPosition) {
         //自由落体运动计算公式
         var dropTime = Math.sqrt(2.0*Math.abs(deltaPosition)/k_Acceleration) * 0.1;
+        // console.log(dropTime)
         //cc.log("dropTime=" + dropTime);
         if (this.initWithDuration(dropTime))
         {
             this.m_positionDeltaY = deltaPosition;
+            // console.log(this.m_positionDeltaY , deltaPosition)
             return true;
         }
-         //cc.log("dropTime =" + dropTime + "; deltaPosition=" + deltaPosition);
+        cc.log("dropTime =" + dropTime + "; deltaPosition=" + deltaPosition);
         return false;
     },
-
-    isDone:function() {
+    isDone:function() {//函数设置了动作是否运行结束。
         if (this.m_targetPosition.y >= this._target.getPositionY()) {
             return true;
         }
         return false;
     },
-
-
     //Node的runAction函数会调用ActionManager的addAction函数，在ActionManager的addAction函数中会调用Action的startWithTarget，然后在Action类的startWithTarget函数中设置_target的值。
-    startWithTarget:function(target) {
+    startWithTarget:function(target) {//函数由ActionManager调用，设置_target的值
         //cc.log("startWithTarget target=" + target);
         cc.ActionInterval.prototype.startWithTarget.call(this, target);
         this.m_startPosition = target.getPosition();
+        // console.log(this.m_startPosition)
         this.m_targetPosition = cc.p(this.m_startPosition.x, this.m_startPosition.y  - this.m_positionDeltaY);
     },
-
-    update:function(dt) {
+    update:function(dt) {//函数在ActionInterval的step函数中会调用，在这个函数中不断更新精灵的坐标，使用了自由落体计算位移的公式。
         this.timeElasped += dt;
-        //cc.log("isdone=" + this.timeElasped);
-
-        if (this._target && !(this.m_targetPosition.y >= this._target.getPositionY())) {
+        // cc.log("isdone=" + this.timeElasped);
+        if (globalSpr && !(this.m_targetPosition.y >= globalSpr.getPositionY())) {
             var yMoveOffset = 0.5 * k_Acceleration * this.timeElasped * this.timeElasped * 0.3;
             if (cc.ENABLE_STACKABLE_ACTIONS) {
                 var newPos =  cc.p(this.m_startPosition.x, this.m_startPosition.y - yMoveOffset);
                 if (this.m_targetPosition.y > newPos.y) {
                     newPos.y = this.m_targetPosition.y;
-                    this._target.stopAction(this);
+                    globalSpr.stopAction(this);
                 }
                    
-                this._target.setPosition(newPos);
+                globalSpr.setPosition(newPos);
                  
             } else {
-                this._target.setPosition(cc.p(this.m_startPosition.x, this.m_startPosition.y + this.m_positionDeltaY * dt));
+                globalSpr.setPosition(cc.p(this.m_startPosition.x, this.m_startPosition.y + this.m_positionDeltaY * dt));
             }
         }
-    }
-            
+    }           
 });
 
 FreeFall.create = function(deltaPosition) {
-        var ff = new FreeFall();
-        ff.initWithOffset(deltaPosition);
-        return ff; 
-    };
+    var ff = new FreeFall();
+    ff.initWithOffset(deltaPosition);
+    return ff; 
+};
 
 function getRandom(maxSize) {
     return Math.floor(Math.random() * maxSize) % maxSize;
