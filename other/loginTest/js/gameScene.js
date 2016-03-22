4fec8d0d867d0c449fe292cb85581040149daf4d
zhong0902 <785861210@qq.com>
2016-03-22 14:53:19 +0800

    1

--- other/loginTest/js/gameScene.js
+++ other/loginTest/js/gameScene.js
@@ -13,9 +13,21 @@ var beginScene = cc.Scene.extend({
 
         var lLayer = new loginLayer();
         this.addChild(lLayer);
+
+
+        this.bg = new Background(res.gameBg);
+        this.bg.attr({
+          x:0,
+          y:0,
+          anchorX:0,
+          anchorY:0
+        });
+        this.addChild(this.bg,0);
+
+        
     },
     update:function(){
-
+        this.bg.update(0,-GC.h,0);
     }
 });
 
@@ -25,14 +37,16 @@ var backGroundLayer = cc.LayerColor.extend({
 
         this.setColor(aColor);
 
-        var background = base.addSpriteToScene(this, res.background, size.width/2, size.height/2);
+        var background = new addSprite(this, {
+            "x" : size.width/2,
+            "y" : size.height/2,
+            "image" : res.background,
+        });
 
         var infinite_rotate = new cc.repeatForever(new cc.rotateBy(60, 360));
         background.runAction(infinite_rotate);
 
-        // base.addUITextToScene(this, "test", 20, size.width/2, size.height/2);
-
-        base.addVedio(this);
+        baseButton.addVedio(this);
     }
 });
 
@@ -41,27 +55,38 @@ var loginLayer = cc.Layer.extend({
     ctor:function(){
         this._super();
 
-        // base.addUITextToScene(this, "注册", 36, size.width/2, size.height/2);
-        var test = new addSpriteToSceneWithEvent(this, {
+        var testSrite = new addSprite(this, {
             "x" : size.width/2,
             "y" : size.height/2,
-            "image" : res.music,
+            "image" : res.canvas,
+            "rect" : true,
+            "animation" : {
+                "todo"    : true,
+                "length"  : 8,
+                "time"    : 0.2,
+                "start_w" : 80,
+                "start_h" : 0,
+                "width"   : 80,
+                "height"  : 80,
+                "returnFirst" : true,
+                "repeat"  : false
+            },
             "listener" :true
         });
 
-        // var loginEditBox = base.addEditBoxFixedToLayer(this, {
-        //     "width" : size.width*0.3,
-        //     "height": size.height*0.12,
-        //     "image" : res.editbox,
-        //     "position" : cc.p(size.width/2, size.height/2),
-        //     "type" : cc.EDITBOX_INPUT_FLAG_PASSWORD,
-        //     "fontsize" : 20,
-        //     "placeHolder" : "please enter password"
-        // }, function(){
-        //     console.log("changeCall");
-        // }, function(){
-        //     console.log("enterCall");
-        // });
+        var loginEditBox = baseButton.addEditBoxFixedToLayer(this, {
+            "width" : size.width*0.3,
+            "height": size.height*0.12,
+            "image" : res.editbox,
+            "position" : cc.p(size.width/2, size.height/2),
+            "type" : cc.EDITBOX_INPUT_FLAG_PASSWORD,
+            "fontsize" : 20,
+            "placeHolder" : "please enter password"
+        }, function(){
+            console.log("changeCall");
+        }, function(){
+            console.log("enterCall");
+        });
     }
 });
 
