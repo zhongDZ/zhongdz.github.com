8e47568a71931c5001c603fa69b6dae7e662993c
zhong0902 <785861210@qq.com>
2016-03-30 15:07:04 +0800

    add

--- gameRoom/scene1/js/gameScene.js
+++ gameRoom/scene1/js/gameScene.js
@@ -11,6 +11,7 @@ var curScene = null;
 var glbalScene = null;
 var _Layer9 = null;
 var _Layer10 = null;
+var showZW = false;
 var desArr = [
     res.describle1,
     res.describle2,
@@ -81,7 +82,7 @@ var gameScene = cc.Scene.extend({
 
             },
             onTouchEnded: function (touch, event) {
-                if (canChangePage) {
+                if (canChangePage && !showZW) {
                     var delta = touch.getLocation().x - this.startPosX;
                     if(self.page1 && self.currentIndex < self.sceneList.length - 1){
                         self.page1 = false;
@@ -118,6 +119,8 @@ var gameScene = cc.Scene.extend({
         if(scene){
             scene.visible = false;
             this.sceneList[index].visible = true;
+            this.sceneList[index].removeAction();
+            this.sceneList[index].initAction();
         }
     },
     initBottom:function(){
@@ -166,6 +169,8 @@ var Layer1 = cc.Layer.extend({
 
         // this.scheduleUpdate();
     },
+    initAction:function(){},
+    removeAction:function(){},
     update:function(){
         // this.tick++;
     },
@@ -176,15 +181,17 @@ var Layer1 = cc.Layer.extend({
         var aniSprObjTag = 3;
         var texture0 = cc.textureCache.addImage(res.huo1);//添加纹理到缓存
         var texture1 = cc.textureCache.addImage(res.huo2);//添加纹理到缓存
-        var aniSpr = new cc.SpriteFrame(texture0,cc.rect(0,0,640,435));//注意 new  别缺少了!!!!!!!!!
+        var texture2 = cc.textureCache.addImage(res.huo3);//添加纹理到缓存
+        var aniSpr = new cc.SpriteFrame(texture0,cc.rect(0,0,640,452));//注意 new  别缺少了!!!!!!!!!
         var aniSprObj = new cc.Sprite(aniSpr);
         this.addChild(aniSprObj,1,aniSprObjTag);
         aniSprObj.anchorY = 0;
         aniSprObj.x = size.width/2;
         aniSprObj.y = 0;
         var sprFrames = [];
-        sprFrames[0] = new cc.SpriteFrame(texture0,cc.rect(0,0,640,435));
-        sprFrames[1] = new cc.SpriteFrame(texture1,cc.rect(0,0,640,435));
+        sprFrames[0] = new cc.SpriteFrame(texture0,cc.rect(0,0,640,452));
+        sprFrames[1] = new cc.SpriteFrame(texture1,cc.rect(0,0,640,452));
+        sprFrames[2] = new cc.SpriteFrame(texture2,cc.rect(0,0,640,452));
         var animation = new cc.Animation(sprFrames,0.6);
         var animate = new cc.animate(animation);
         var repeat = animate.repeatForever();
@@ -203,6 +210,27 @@ var Layer2 = cc.Layer.extend({
         this.addChild(this.layer1);
 
         this.addAni();
+    },
+    initAction:function(){
+        this.zi2 = new cc.Sprite(res.zi2);
+        this.zi2.x = size.width/2 * 0;
+        this.zi2.y = size.height/2 * 0;
+        this.addChild(this.zi2, 10);
+
+        var mt = new cc.moveTo(1.5,cc.p(size.width/2 - 100, size.height - 180));
+        var mtCall = new cc.callFunc(function(){
+            this.zi2.runAction(repeat);
+        },this);
+        var seq = new cc.sequence(mt, mtCall);
+
+        var rb = new cc.rotateBy(3,5);
+        var Rrb = rb.reverse();
+        var repeat = new cc.sequence(rb,Rrb).repeatForever();
+
+        this.zi2.runAction(seq)
+    },
+    removeAction:function(){
+        this.removeChild(this.zi2);
     },
     addAni:function(){
         var aniSprObjTag = 3;
@@ -236,6 +264,27 @@ var Layer3 = cc.Layer.extend({
 
         this.addAni();
     },
+    initAction:function(){
+        this.zi3 = new cc.Sprite(res.zi3);
+        this.zi3.x = size.width/2;
+        this.zi3.y = size.height/2 * 0;
+        this.addChild(this.zi3, 10);
+
+        var mt = new cc.moveTo(0.8,cc.p(size.width/2, size.height - 160));
+        var mtCall = new cc.callFunc(function(){
+            this.zi3.runAction(repeat);
+        },this);
+        var seq = new cc.sequence(mt, mtCall);
+
+        var rb = new cc.rotateBy(3,5);
+        var Rrb = rb.reverse();
+        var repeat = new cc.sequence(rb,Rrb).repeatForever();
+
+        this.zi3.runAction(seq);
+    },
+    removeAction:function(){
+        this.removeChild(this.zi3);
+    },
     addAni:function(){
         var aniSprObjTag = 3;
         var texture0 = cc.textureCache.addImage(res.p1);//添加纹理到缓存
@@ -264,20 +313,42 @@ var Layer4 = cc.Layer.extend({
         this.layer1.x = size.width/2;
         this.layer1.y = size.height/2;
         this.addChild(this.layer1);
+    },
+    initAction:function(){
+        this.zi4 = new cc.Sprite(res.zi4);
+        this.zi4.x = size.width/2;
+        this.zi4.y = -size.height/2;
+        this.addChild(this.zi4, 10);
 
-        var tShirt = new cc.Sprite(res.tShirt);
+        var mt = new cc.moveTo(1.5,cc.p(size.width/2, size.height - 160));
+        var mtCall = new cc.callFunc(function(){
+            this.zi4.runAction(repeat);
+        },this);
+        var seq = new cc.sequence(mt, mtCall);
+
+        var rb = new cc.rotateBy(3,5);
+        var Rrb = rb.reverse();
+        var repeat = new cc.sequence(rb,Rrb).repeatForever();
+
+        this.zi4.runAction(seq)
+
+        var tShirt = this.tShirt = new cc.Sprite(res.tShirt);
         tShirt.x = size.width/2;
-        tShirt.y = size.height/2 - 100;
+        tShirt.y = size.height * 3/2;
         this.addChild(tShirt);
 
-        var _this = this;
-        var actionBy = new cc.SkewBy(2, 180, 0);
+        var aMt = new cc.moveTo(2,cc.p(size.width/2,size.height/2 - 100));
         var actionByCall = new cc.callFunc(function(){
-            _this.addAni();
+            this.addAni();
         },this);
-        var seq = new cc.sequence(actionBy,actionByCall);
+        var seq = new cc.sequence(aMt,actionByCall);
 
         tShirt.runAction(seq);
+    },
+    removeAction:function(){
+        this.removeChild(this.zi4);
+        this.removeChild(this.tShirt);
+        this.removeChild(this.aniSpr);
     },
     addAni:function(){
         var aniSprObjTag = 3;
@@ -286,7 +357,7 @@ var Layer4 = cc.Layer.extend({
         var texture2 = cc.textureCache.addImage(res.s3);//添加纹理到缓存
         var texture3 = cc.textureCache.addImage(res.s4);//添加纹理到缓存
         var aniSpr = new cc.SpriteFrame(texture0,cc.rect(0,0,470,479));//注意 new  别缺少了!!!!!!!!!
-        var aniSprObj = new cc.Sprite(aniSpr);
+        var aniSprObj = this.aniSpr = new cc.Sprite(aniSpr);
         this.addChild(aniSprObj,1,aniSprObjTag);
         aniSprObj.anchorY = .5;
         aniSprObj.x = size.width/2 + 20;
@@ -312,27 +383,57 @@ var Layer5 = cc.Layer.extend({
         this.layer1.x = size.width/2;
         this.layer1.y = size.height/2;
         this.addChild(this.layer1);
+    },
+    initAction:function(){
+        this.zi5 = new cc.Sprite(res.zi5);
+        this.zi5.x = size.width/2;
+        this.zi5.y = size.height - 200;
+        this.addChild(this.zi5, 10);
+        this.zi5.opacity = 0;
+
+        var fi = new cc.FadeIn(1.5);
+        var fiCall = new cc.callFunc(function(){
+            this.zi5.runAction(rbRrb)
+        },this);
+        var rb = new cc.rotateBy(3,5);
+        var Rrb = rb.reverse();
+        var rbRrb = new cc.sequence(rb,Rrb).repeatForever();
+        var seq = new cc.sequence(fi,fiCall)
+        this.zi5.runAction(seq);
+
+        var l = this.l = new cc.Layer();
+        l.anchorX = 0;
+        l.anchorY = 0;
+        l.x = -300;
+        l.y = 0;
+        this.addChild(l);
+        var lMt = new cc.moveTo(5,cc.p(0,0));
+        l.runAction(lMt);
 
         var bike = new cc.Sprite(res.bike);
         bike.x = size.width/2;
         bike.y = 250;
-        this.addChild(bike,10);
+        l.addChild(bike,10);
 
         var lRound = new cc.Sprite(res.l);
         lRound.x = size.width/2 - 132;
         lRound.y = 210;
-        this.addChild(lRound);
+        l.addChild(lRound);
 
         var rRound = new cc.Sprite(res.r);
         rRound.x = size.width/2 + 200;
         rRound.y = 200;
-        this.addChild(rRound);
+        l.addChild(rRound);
 
         var rb = new cc.rotateBy(6,360);
         var Rrb = rb.repeatForever();
 
         lRound.runAction(Rrb.clone());
         rRound.runAction(Rrb.clone());
+    },
+    removeAction:function(){
+        this.removeChild(this.zi5);
+        this.removeChild(this.l);
     }
 });
 var Layer6 = cc.Layer.extend({
@@ -345,8 +446,25 @@ var Layer6 = cc.Layer.extend({
         this.layer6.x = size.width/2;
         this.layer6.y = size.height/2;
         this.addChild(this.layer6);
+    },
+    initAction:function(){
+        this.zi6 = new cc.Sprite(res.zi6);
+        this.zi6.x = size.width/2;
+        this.zi6.y = -size.height/2;
+        this.addChild(this.zi6, 10);
 
-        var shine = new cc.Sprite(res.shine);
+        var mt = new cc.moveTo(0.8,cc.p(size.width/2, size.height - 160));
+        var mtCall = new cc.callFunc(function(){
+            var rb = new cc.rotateBy(3,5);
+            var Rrb = rb.reverse();
+            var repeat = new cc.sequence(rb,Rrb).repeatForever();
+            this.zi6.runAction(repeat);
+        },this);
+        var seq = new cc.sequence(mt, mtCall);
+
+        this.zi6.runAction(seq)
+
+        var shine = this.shine = new cc.Sprite(res.shine);
         shine.anchorX = 1;
         shine.x = size.width + 35;
         shine.y = size.height/2 + 15;
@@ -357,12 +475,12 @@ var Layer6 = cc.Layer.extend({
         var seq = new cc.sequence(fo,fi).repeatForever();
         shine.runAction(seq);
 
-        var lRound = new cc.Sprite(res.ll);
+        var lRound = this.lRound = new cc.Sprite(res.ll);
         lRound.x = size.width/2 - 130;
         lRound.y = size.height/2 + 130;
         this.addChild(lRound);
 
-        var rRound = new cc.Sprite(res.rr);
+        var rRound = this.rRound = new cc.Sprite(res.rr);
         rRound.x = size.width/2 + 35;
         rRound.y = size.height/2 + 125;
         this.addChild(rRound);
@@ -372,6 +490,12 @@ var Layer6 = cc.Layer.extend({
 
         lRound.runAction(Rrb.clone());
         rRound.runAction(Rrb.clone());
+    },
+    removeAction:function(){
+        this.removeChild(this.zi6);
+        this.removeChild(this.shine);
+        this.removeChild(this.lRound);
+        this.removeChild(this.rRound);
     }
 });
 var Layer7 = cc.Layer.extend({
@@ -384,28 +508,47 @@ var Layer7 = cc.Layer.extend({
         this.layer7.x = size.width/2;
         this.layer7.y = size.height/2;
         this.addChild(this.layer7);
-
-        this.addAni();
     },
-    addAni:function(){
-        var aniSprObjTag = 3;
-        var texture0 = cc.textureCache.addImage(res.e1);//添加纹理到缓存
-        var texture1 = cc.textureCache.addImage(res.e2);//添加纹理到缓存
-        var texture2 = cc.textureCache.addImage(res.e3);//添加纹理到缓存
-        var aniSpr = new cc.SpriteFrame(texture0,cc.rect(0,0,119,119));//注意 new  别缺少了!!!!!!!!!
-        var aniSprObj = new cc.Sprite(aniSpr);
-        this.addChild(aniSprObj,1,aniSprObjTag);
-        aniSprObj.anchorY = .5;
-        aniSprObj.x = size.width/2 - 45;
-        aniSprObj.y = size.height - 230;
-        var sprFrames = [];
-        sprFrames[0] = new cc.SpriteFrame(texture0,cc.rect(0,0,119,119));
-        sprFrames[1] = new cc.SpriteFrame(texture1,cc.rect(0,0,119,119));
-        sprFrames[2] = new cc.SpriteFrame(texture2,cc.rect(0,0,119,119));
-        var animation = new cc.Animation(sprFrames,0.6);
-        var animate = new cc.animate(animation);
-        var repeat = animate.repeatForever();
-        aniSprObj.runAction(animate);
+    initAction:function(){
+        var erhuan = this.erhuan = new cc.Sprite(res.erhuan);
+        erhuan.anchorY = 1;
+        erhuan.x = size.width/2 - 18;
+        erhuan.y = size.height - 200;
+        this.addChild(erhuan);
+
+        var erhuan1 = this.erhuan1 = new cc.Sprite(res.erhuan);
+        erhuan1.anchorY = 1;
+        erhuan1.x = size.width/2 - 70;
+        erhuan1.y = size.height - 200;
+        this.addChild(erhuan1);
+
+        var rb = new cc.rotateBy(3,10);
+        var Rrb = rb.reverse();
+        var repeat1 = new cc.sequence(rb,Rrb).repeatForever();
+        erhuan.runAction(repeat1);
+        erhuan1.runAction(repeat1.clone());
+
+        this.zi7 = new cc.Sprite(res.zi7);
+        this.zi7.x = size.width/2;
+        this.zi7.y = -size.height/2;
+        this.addChild(this.zi7, 10);
+
+        var mt = new cc.moveTo(0.8,cc.p(size.width/2 + 150, size.height - 160));
+        var mtCall = new cc.callFunc(function(){
+            this.zi7.runAction(repeat);
+        },this);
+        var seq = new cc.sequence(mt, mtCall);
+
+        var rb = new cc.rotateBy(3,15);
+        var Rrb = rb.reverse();
+        var repeat = new cc.sequence(rb,Rrb).repeatForever();
+
+        this.zi7.runAction(seq)
+    },
+    removeAction:function(){
+        this.removeChild(this.erhuan);
+        this.removeChild(this.erhuan1);
+        this.removeChild(this.zi7);
     }
 });
 var Layer8 = cc.Layer.extend({
@@ -450,6 +593,7 @@ var Layer8 = cc.Layer.extend({
         down.runAction(repeat2);
 
         var menuItem = new cc.MenuItemSprite(new cc.Sprite(res.taijie),new cc.Sprite(res.taijie),function(){
+                if(showZW)return;
                 var atick = setInterval(function(){
                     menu.y -= 5;
                     if(menu.y <= size.height/2 - 280){
@@ -463,6 +607,8 @@ var Layer8 = cc.Layer.extend({
                         menu.y = size.height/2 - 250;
                     }
                 },100);
+
+                showZW = true;
         },this);
         var menu = new cc.Menu(menuItem);
         menu.x = size.width/2 - 60;
@@ -484,7 +630,9 @@ var Layer8 = cc.Layer.extend({
         floor.x = size.width/2;
         floor.y = 0;
         this.addChild(floor,1);
-    }
+    },
+    initAction:function(){},
+    removeAction:function(){}
 });
 var Layer9 = cc.Layer.extend({
     ctor: function () {
@@ -497,28 +645,28 @@ var Layer9 = cc.Layer.extend({
         this.layer9.y = size.height/2;
         this.addChild(this.layer9);
 
-        //var info = [
-        //    {"res":res.zhiwei1,"x":size.width/2 - 100,"y":size.height/2 + 270},
-        //    {"res":res.zhiwei2,"x":size.width/2 + 100,"y":size.height/2 + 270},
-        //    {"res":res.zhiwei3,"x":size.width/2 - 190,"y":size.height/2 + 20},
-        //    {"res":res.zhiwei4,"x":size.width/2,       "y":size.height/2 + 20},
-        //    {"res":res.zhiwei5,"x":size.width/2 + 190,"y":size.height/2 + 20},
-        //    {"res":res.zhiwei6,"x":size.width/2 - 100,"y":size.height/2 - 240},
-        //    {"res":res.zhiwei7,"x":size.width/2 + 100,"y":size.height/2 - 240},
-        //];
-
         var info = [
-            {"res":res.zhiwei1,"x":size.width/2 - 100,"y":size.height/2 + 270},
-            {"res":res.zhiwei2,"x":size.width/2 + 100,"y":size.height/2 + 270},
-            {"res":res.zhiwei3,"x":size.width/2 - 190,"y":size.height/2 + 20},
-            {"res":res.zhiwei4,"x":size.width/2,       "y":size.height/2 + 20},
-            {"res":res.zhiwei5,"x":size.width/2 + 190,"y":size.height/2 + 20},
-            {"res":res.zhiwei6,"x":size.width/2 - 100,"y":size.height/2 - 240},
-            {"res":res.zhiwei7,"x":size.width/2 + 100,"y":size.height/2 - 240},
+            {"res":res.zhiwei1,"res1":res.zhiwei11,"x":size.width/2 - 100,"y":size.height/2 + 270},
+            {"res":res.zhiwei2,"res1":res.zhiwei22,"x":size.width/2 + 100,"y":size.height/2 + 270},
+            {"res":res.zhiwei3,"res1":res.zhiwei33,"x":size.width/2 - 190,"y":size.height/2 + 20},
+            {"res":res.zhiwei4,"res1":res.zhiwei44,"x":size.width/2,       "y":size.height/2 + 20},
+            {"res":res.zhiwei5,"res1":res.zhiwei55,"x":size.width/2 + 190,"y":size.height/2 + 20},
+            {"res":res.zhiwei6,"res1":res.zhiwei66,"x":size.width/2 - 100,"y":size.height/2 - 240},
+            {"res":res.zhiwei7,"res1":res.zhiwei77,"x":size.width/2 + 100,"y":size.height/2 - 240},
         ];
 
+        var info1 = [
+            {"x":93,"y":140},
+            {"x":85,"y":140},
+            {"x":80,"y":180},
+            {"x":80,"y":150},
+            {"x":80,"y":160},
+            {"x":80,"y":180},
+            {"x":85,"y":160}
+        ]
+
         for(var i=0;i<info.length;i++){
-            var spr = new TouchSpr(info[i].res, i);
+            var spr = new TouchSpr(info[i].res, i, info[i].res1, info1[i].x, info1[i].y);
             spr.x = info[i].x;
             spr.y = info[i].y;
             this.addChild(spr,1)
@@ -526,6 +674,8 @@ var Layer9 = cc.Layer.extend({
 
         var backItem = new cc.MenuItemSprite(new cc.Sprite(res.back),new cc.Sprite(res.back),function(){
                 glbalScene.removeChild(_Layer9);
+
+                showZW = false;
         },this);
         var backMenu = new cc.Menu(backItem);
         backMenu.x = size.width/2;
@@ -597,13 +747,22 @@ var Layer12 = cc.Layer.extend({
 
 
 var TouchSpr = cc.Sprite.extend({
-    ctor:function(res, flag){
-        this._super(res, flag);
+    ctor:function(res, flag, res1, x1, y1){
+        this._super(res, flag, res1, x1, y1);
         var self = this;
 
         this.initWithFile(res);
         this.scale = 0.8;
         this.flag = flag;
+
+        var spr = new cc.Sprite(res1);
+        spr.x = x1;
+        spr.y = y1;
+        this.addChild(spr);
+        spr.scale = 0.1;
+
+        var st = new cc.scaleTo(1,1);
+        spr.runAction(st);
 
         this.loadListener();
     },
