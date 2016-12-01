77ed5bea2949bb1d72f095cd4e3d5b78d859cadf
zhongdezong <785861210@qq.com>
2016-12-01 18:03:17 +0800

    更改 获取 github issues配置、更新项目展示页

--- wap_office/js/config.js
+++ wap_office/js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
     owner        : 'zhongDZ',
-    repo         : 'zhongdz',
+    repo         : 'zhongdz.github.com',
     duoshuo      : 'zhongdz',
     // baiduAnaly   : '9c3bc559412385c286e7caa93bdbbc04',
     access_token : 'bea8512e2223f8833727d301dced2adb874313d5'
