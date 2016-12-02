757eefb61c1e901dcb97620a1cb4c4417583ee5b
zhongdezong <785861210@qq.com>
2016-12-02 10:41:45 +0800

    update

---
+++ wap_office/js/config.js
@@ -0,0 +1,9 @@
+var CONFIG = {
+    owner        : 'zhongDZ',
+    repo         : 'zhongdz.github.com',
+    duoshuo      : 'zhongdz',
+    // baiduAnaly   : '9c3bc559412385c286e7caa93bdbbc04',
+    access_token : 'bea8512e2223f8833727d301dced2adb874313d5'
+};
+
+var duoshuoQuery = {short_name: CONFIG['duoshuo']};
