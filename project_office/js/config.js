062cbbc6e45c36b301d80cb012047c9caacb8bf7
zhongdezong <785861210@qq.com>
2017-02-10 18:14:51 +0800

    delete

--- project_office/js/config.js
+++
@@ -1,9 +0,0 @@
-var CONFIG = {
-    owner        : 'zhongDZ',
-    repo         : 'zhongdz.github.com',
-    duoshuo      : 'zhongdz',
-    // baiduAnaly   : '9c3bc559412385c286e7caa93bdbbc04',
-    access_token : 'bea8512e2223f8833727d301dced2adb874313d5'
-};
-
-var duoshuoQuery = {short_name: CONFIG['duoshuo']};
